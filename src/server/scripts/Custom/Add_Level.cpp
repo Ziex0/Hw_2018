fa0cd1047e4743105843c12a622d8c4990c62f9e
Zie <andra778@yahoo.com>
2015-03-10 20:17:02 +0100

    update level token and update offset port

--- src/server/scripts/Custom/Add_Level.cpp
+++ src/server/scripts/Custom/Add_Level.cpp
@@ -4,7 +4,7 @@
     enum
     {
     item_id = 320277,
-    max_lvl = 254,
+    max_lvl = 255,
      };
      
     class Level_item : public ItemScript
@@ -14,14 +14,14 @@ public:
 
     bool OnUse(Player* player, Item* item, SpellCastTargets const& /*targets*/)
     {
-        if  (player->getLevel() == 254)
+        if  (player->getLevel() == 255)
         {
-            player->GetSession()->SendNotification("You already have level 254.");
+            player->GetSession()->SendNotification("You already have level 255.");
             return false;
         }
-        player->GiveLevel(254);
+        player->GiveLevel(255);
         player->DestroyItemCount(item->GetEntry(),1,true,false);
-        player->GetSession()->SendNotification("Your level was set to 254.");
+        player->GetSession()->SendNotification("Your level was set to 255.");
         return true;
     }
 };
